c3c7b7265da5664e357b607d30ce0147082dd42a
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-05-22 06:02:41 +0600

    Update May-18-9.py
    Комментарий преподавателя

--- May-18-9.py
+++ May-18-9.py
@@ -1,5 +1,5 @@
 number = int(input("Введите пятизначное число: "))
 last = number % 10
-number /= 10
+number /= 10   # VN: здесь нужно целочисленное деление //=
 new_number = last * 10000 + number
 print(new_number)
