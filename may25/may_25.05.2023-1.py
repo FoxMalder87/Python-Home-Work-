8d0513a983ca6f37044b79e55323c1ebbb5c1b7a
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-06-04 23:41:54 +0600

    Проверено дз от 25.05
    Комментарии к коду

--- may25/may_25.05.2023-1.py
+++ may25/may_25.05.2023-1.py
@@ -1,4 +1,5 @@
 try:
+    # VN: input не нужно помещать в try! Вычисления - тоже, кроме деления. Это касается всех задач
     user_input = input("Введите число: ")
     user_int = int(user_input)
 except ValueError:
--- may25/may_25.05.2023-8.py
+++ may25/may_25.05.2023-8.py
@@ -1,6 +1,8 @@
 try:
     Enter_a_three_digit_number = int(input("Введите трехзначное число: "))
     if Enter_a_three_digit_number < 100 or Enter_a_three_digit_number > 999:
+    # VN: Здорово, что вы разобрались с raise. В будущем его так не используйте.
+    # т.е. число не находится в диапазоне - это не исключительная ситуация
         raise ValueError("Ошибка: Введено число, не являющееся трехзначным.")
     division = (Enter_a_three_digit_number / 10) % 10
 except ValueError as e:
